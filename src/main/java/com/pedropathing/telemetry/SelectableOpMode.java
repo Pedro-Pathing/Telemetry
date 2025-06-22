105b70450c47e80dd3b808040c2477131c2cce3c
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-06-22 13:41:40 -0500

    fix: make SelectableOpMode run init and init_loop on the selected OpMode

--- src/main/java/com/pedropathing/telemetry/SelectableOpMode.java
+++ src/main/java/com/pedropathing/telemetry/SelectableOpMode.java
@@ -16,7 +16,10 @@ public abstract class SelectableOpMode extends OpMode {
 
     public SelectableOpMode(String name, Consumer<SelectScope<Supplier<OpMode>>> opModes) {
         selector = Selector.create(name, opModes, MESSAGE);
-        selector.onSelect(opModeSupplier -> selectedOpMode = opModeSupplier.get());
+        selector.onSelect(opModeSupplier -> {
+            selectedOpMode = opModeSupplier.get();
+            selectedOpMode.init();
+        });
     }
 
     @Override
@@ -25,14 +28,16 @@ public abstract class SelectableOpMode extends OpMode {
 
     @Override
     public final void init_loop() {
-        if (gamepad1.dpad_up || gamepad2.dpad_up) selector.decrementSelected();
-        else if (gamepad1.dpad_down || gamepad2.dpad_down) selector.incrementSelected();
-        else if (gamepad1.right_bumper || gamepad2.right_bumper) selector.select();
-        else if (gamepad1.left_bumper || gamepad2.left_bumper) selector.goBack();
+        if (selectedOpMode == null) {
+            if (gamepad1.dpad_up || gamepad2.dpad_up) selector.decrementSelected();
+            else if (gamepad1.dpad_down || gamepad2.dpad_down) selector.incrementSelected();
+            else if (gamepad1.right_bumper || gamepad2.right_bumper) selector.select();
+            else if (gamepad1.left_bumper || gamepad2.left_bumper) selector.goBack();
 
-        for (String line : selector.getLines()) {
-            telemetry.addLine(line);
-        }
+            for (String line : selector.getLines()) {
+                telemetry.addLine(line);
+            }
+        } else selectedOpMode.init_loop();
     }
 
     @Override
